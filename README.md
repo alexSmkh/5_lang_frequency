d0087039f758f55487c96855a324af102553735d
alexSmkh <alexshikari9333@gmail.com>
2018-10-05 10:35:38 +0300

    first commit

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
-# Frequency Analysis of Words
+# Частота слов
 
-[TODO. There will be project description]
+Скрипт принимает текстовый файл и выводит 10 самых популярных слов
 
-# Project Goals
+#Пример запуска
+```bash
+python lang_frequency.py Orwell_George_-_1984.txt
+и 626
+в 524
+не 370
+на 287
+он 281
+что 251
+с 209
+но 132
+уинстон 131
+а 123
+```
 
+#Project goals
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,33 @@
+import sys
+import os
+import re
+import collections
 
 
-def load_data(filepath):
-    pass
+def load_data(file_name):
+    if not os.path.exists(file_name):
+        return None
+    with open(file_name, 'r') as file_handler:
+        return file_handler.read()
 
 
 def get_most_frequent_words(text):
-    pass
+    words = re.findall(r"\w+", text.lower())
+    return collections.Counter(words).most_common(10)
+
+
+def get_text_file_name():
+    name = sys.argv[1]
+    return name
+
+
+def print_most_frequent_words(list_of_words):
+    for word, num in list_of_words:
+        print(word, num)
 
 
 if __name__ == '__main__':
-    pass
+    text_file = get_text_file_name()
+    text_from_file = load_data(text_file)
+    most_frequent_words = get_most_frequent_words(text_from_file)
+    print_most_frequent_words(most_frequent_words)
